5ec7c1adffdaa506e5d6320ecd0c36c2ecb560d8
Tanay Raundale <tanayraundale13@gmail.com>
2024-03-23 14:34:04 +0530

    Update margin.py

--- mainproject/margin.py
+++ mainproject/margin.py
@@ -1,5 +1,15 @@
 from docx import Document
 from docx.shared import Pt
+import pyttsx3
+import os
+
+def speak(audio):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voices', voices[0].id)
+    engine.setProperty('rate', 160)
+    engine.say(audio)
+    engine.runAndWait()
 
 
 def set_margins(doc, top, right, bottom, left):
@@ -10,23 +20,24 @@ def set_margins(doc, top, right, bottom, left):
         section.right_margin = Pt(right)
         section.bottom_margin = Pt(bottom)
         section.left_margin = Pt(left)
+    speak("Margin Added Successfully")
 
-def main():
 
-    doc = Document("cloud.docx")   # Create a new Word document
+def add_margins(docname):
+
+    doc = Document(docname)   # Create a new Word document
 
     # Set margins (in points)
-    top_margin = 2.54  # 1/2 inch
-    right_margin = 1.5
-    bottom_margin = 2.86
-    left_margin = 3.81
+    top_margin = 1  # 1/2 inch
+    right_margin = 1.2
+    bottom_margin = 1.75                
+    left_margin = 1.00
 
     set_margins(doc, top_margin, right_margin, bottom_margin, left_margin)
 
     # doc.add_heading('Document generated with Default Margins CM', level=1)
     # doc.add_paragraph("")
     # doc.add_paragraph('This is an Simple Default Margin Example and generates Document...')
-    doc.save("Applied_margin.docx")
-
-if __name__ == "__main__":
-    main()
+    speak("I will open the modified document for you")
+    doc.save(docname)
+    os.startfile(f"C:\\Users\\Dell\\Desktop\\final year project\\mainproject\\{docname}") 
